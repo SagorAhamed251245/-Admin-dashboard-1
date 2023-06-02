a6e668be3a37425aa5cb36bcfe6bfaf7962ec2ef
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-06-02 11:22:35 +0600

    scorecards done

--- src/Components/Dashboard/Scorecards/Scorecards.jsx
+++ src/Components/Dashboard/Scorecards/Scorecards.jsx
@@ -1,10 +1,82 @@
 
+
+import { FaArrowCircleUp } from 'react-icons/fa';
+import {
+    ResponsiveContainer,
+    LineChart,
+    Line
+} from "recharts";
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
 const Scorecards = () => {
+
+
     return (
         <div className='bg-red-200 col-span-1 p-3'>
-          <div>
-          <p>Convertionrate</p>
-          </div>
+            <div>
+                <p className='font-bold text-base mb-2'>Convertionrate</p>
+            </div>
+            <div>
+                <div className='flex  w-full justify-between items-center font-semibold mb-2'>
+                <p>Today</p>
+                <p className='flex  gap-2 items-center'>30% <FaArrowCircleUp className='text-green-500'></FaArrowCircleUp></p>
+                </div>
+                <div>
+                    <p className='text-2xl font-bold'>19.5%</p>
+                </div>
+
+            </div>
+
+            <div className='my-container flex items-center justify-center'>
+                <ResponsiveContainer width="100%" height={50}>
+                    <LineChart width="100%" height={100} data={data}>
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+            
 
         </div>
     );
--- src/Components/Dashboard/Statistics/Statistics.jsx
+++ src/Components/Dashboard/Statistics/Statistics.jsx
@@ -3,7 +3,11 @@
 const Statistics = () => {
     return (
         <div className='bg-green-200 col-span-3 row-span-2'>
-            Statistics
+            <div>
+                <div>Visit</div>
+                <div>Order</div>
+                <div>refund</div>
+            </div>
         </div>
     );
 };
